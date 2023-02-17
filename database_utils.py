a41b2ce69f24001e096297e6d93aa39084ff1a06
“luca-saccilotto” <luca.sacc@outlook.com>
2023-02-17 20:58:58 +0100

    Completes "Task 3 - Step 3"

--- database_utils.py
+++ database_utils.py
@@ -1,2 +1,27 @@
+# Import libraries
+from sqlalchemy import create_engine
+import yaml
+
 class DatabaseConnector:
+
+    # Create a method that will read the credentials and return a dictionary
+    def read_db_creds(self):
+        with open("../db_creds.yaml", "r") as f:
+            credentials = yaml.safe_load(f)
+            return credentials
     
+    # Create a method that will initialise and return a database engine
+    def init_db_engine(self):
+        credentials = self.read_db_creds()
+        
+        """Use the dictionary to set variables"""
+        DATABASE_TYPE = "postgresql"
+        DBAPI = "psycopg2"
+        HOST = credentials["RDS_HOST"]
+        USER = credentials["RDS_USER"]
+        PASSWORD = credentials["RDS_PASSWORD"]
+        DATABASE = credentials["RDS_DATABASE"]
+        PORT = credentials["RDS_PORT"]
+        
+        engine = create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}")
+        return engine
